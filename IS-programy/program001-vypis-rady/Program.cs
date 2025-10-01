e035f006e7e8fdc391270f15bb398e3617e0d4cc
Simon Janousek <simon.janousek@tul.cz>
2025-10-01 10:34:40 +0200

    #3 ukol

---
+++ IS-programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,30 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*********************");
+    Console.WriteLine("********** Výpis řady **********");
+    Console.WriteLine("************************");
+    Console.WriteLine("************ SJ *************");
+    Console.WriteLine("*********************");
+    Console.WriteLine();
+
+
+    //Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.WriteLine("Zadejte první číslo řady (celé číslo): ");
+    int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first)) 
+    {
+        Console.WriteLine("nezadali jste celé číslo, zadejte první číslo řady znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("pro opakování stiskněte a");
+    again = Console.ReadLine();
+
+}
