c95fd5cb30b6155415656286e326d3627907e231
Simon Janousek <simon.janousek@tul.cz>
2025-11-11 22:50:46 +0100

    #N1

---
+++ IS-programy/úkol_obrazec_2/Program.cs
@@ -0,0 +1,42 @@
+﻿static void Main()
+    {
+        string again = "a";
+
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("*******************************");
+            Console.WriteLine("************* N  **************");
+            Console.WriteLine("*******************************");
+            Console.WriteLine("******* Šimon Janoušek ********");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+
+            Console.Write("Zadejte velikost (celé číslo): ");
+
+            int size;
+            while (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size; j++)
+                {
+                    if (j == 1 || j == size || j == i)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+            again = Console.ReadLine().ToLower();
+        }
+    }
+
